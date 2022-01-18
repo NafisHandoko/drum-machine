49f4bd169f976a6fcc7576624604bd1f6d38ba81
Nafis Handoko <naffisioner@gmail.com>
2022-01-18 14:25:40 +0700

    basic style done

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,67 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([
+    {
+      key: 'Q',
+      audioName: 'lorem',
+      audioLink: 'lorem'
+    },
+    {
+      key: 'W',
+      audioName: 'lorem',
+      audioLink: 'lorem'
+    },
+    {
+      key: 'E',
+      audioName: 'lorem',
+      audioLink: 'lorem'
+    },
+    {
+      key: 'A',
+      audioName: 'lorem',
+      audioLink: 'lorem'
+    },
+    {
+      key: 'S',
+      audioName: 'lorem',
+      audioLink: 'lorem'
+    },
+    {
+      key: 'D',
+      audioName: 'lorem',
+      audioLink: 'lorem'
+    },
+    {
+      key: 'Z',
+      audioName: 'lorem',
+      audioLink: 'lorem'
+    },
+    {
+      key: 'X',
+      audioName: 'lorem',
+      audioLink: 'lorem'
+    },
+    {
+      key: 'C',
+      audioName: 'lorem',
+      audioLink: 'lorem'
+    }
+  ]);
 
+  const [display, setDisplay] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="container">
+      <div id="drum-machine">
+        <div id="drum-pads">
+          {data.map((drum) => (
+            <div className="drum-pad">{drum.key}</div>
+          ))}
+        </div>
+        <div className="display-div">
+          <div id="display">text here</div>
+        </div>
+      </div>
     </div>
   )
 }
